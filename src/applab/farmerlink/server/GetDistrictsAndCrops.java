64842e8c9dec602773c239656de25e5f95ad2b94
Eric Kamara <ekamara@gmail.com>
2012-07-04 10:53:30 +0300

    Initial setup of FarmerLink server repository

---
+++ src/applab/farmerlink/server/GetDistrictsAndCrops.java
@@ -0,0 +1,36 @@
+package applab.farmerlink.server;
+
+import applab.server.ApplabServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetDistrictsAndCrops
+ */
+public class GetDistrictsAndCrops extends ApplabServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public GetDistrictsAndCrops() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/applab/farmerlink/server/GetFarmersAndMarketPrices.java
@@ -0,0 +1,36 @@
+package applab.farmerlink.server;
+
+import applab.server.ApplabServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetFarmersAndMarketPrices
+ */
+public class GetFarmersAndMarketPrices extends ApplabServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public GetFarmersAndMarketPrices() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
