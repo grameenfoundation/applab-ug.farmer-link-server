906fbd24037671828ec70fe0617a2743f699c08a
Eric Kamara <ekamara@gmail.com>
2012-07-05 12:57:46 +0300

    1. Added GetDistrictsAndCrops servlet that returns the listing of all the districts and crops in the system 2. Added GetFarmersAndMarketPrices servlet that takes a district and crop and returns the farmers who grow that crop in that district and the markets where we have prices for that crop in that district

--- src/applab/farmerlink/server/GetDistrictsAndCrops.java
+++ src/applab/farmerlink/server/GetDistrictsAndCrops.java
@@ -1,16 +1,32 @@
 package applab.farmerlink.server;
 
+import applab.server.ApplabConfiguration;
 import applab.server.ApplabServlet;
-import java.io.IOException;
-import javax.servlet.ServletException;
+import applab.server.WebAppId;
+
+import java.io.PrintWriter;
+import java.rmi.RemoteException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.rpc.ServiceException;
+
+import com.sforce.soap.enterprise.LoginResult;
+import com.sforce.soap.enterprise.SessionHeader;
+import com.sforce.soap.enterprise.SforceServiceLocator;
+import com.sforce.soap.enterprise.SoapBindingStub;
+import com.sforce.soap.enterprise.fault.InvalidIdFault;
+import com.sforce.soap.enterprise.fault.LoginFault;
+import com.sforce.soap.enterprise.fault.UnexpectedErrorFault;
+import com.sforce.soap.schemas._class.GetDistrictsAndCrops.GetDistrictsAndCropsBindingStub;
+import com.sforce.soap.schemas._class.GetDistrictsAndCrops.GetDistrictsAndCropsServiceLocator;
 
 /**
  * Servlet implementation class GetDistrictsAndCrops
  */
 public class GetDistrictsAndCrops extends ApplabServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String IMEI = "x-Imei";
 
     /**
      * Default constructor. 
@@ -20,17 +36,73 @@ public class GetDistrictsAndCrops extends ApplabServlet {
     }
 
 	/**
+	 * @throws Exception 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        log("Reached get method for get districts and crops");
+        doApplabPost(request, response);
 	}
 
 	/**
+	 * @throws Exception 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		log("Reached method to get the configured districts and crops");
+		String imei = request.getHeader(IMEI);
+		// Make Sales force call
+		String jsonResult = getDistrictsAndCropsFromSalesforce(imei);
+		PrintWriter sendResponse = response.getWriter();
+		sendResponse.println(jsonResult);
+		log("Finished sending districts and crops");
 	}
+
+	private String getDistrictsAndCropsFromSalesforce(String imei) throws Exception {
+		GetDistrictsAndCropsBindingStub serviceStub = setupSalesforceAuthentication();
+		String[] districtsAndCrops = serviceStub.getDistrictsAndCrops();
+		String jsonString = createJson(districtsAndCrops);
+		//"{\"districts\": [\"Abim\", \"Pader\",\"Nwoya\",\"Kitgum\"], \"crops\":[\"Simsim\", \"Beans\",\"Bananas\",\"Maize\"]}";
+		return jsonString;
+	}
+	
+    private String createJson(String[] districtsAndCrops) {
+        StringBuffer districtsAndCropsJson = new StringBuffer();
+        districtsAndCropsJson.append("{\"districts\" : ");
+        districtsAndCropsJson.append(districtsAndCrops[0]);
+        districtsAndCropsJson.append(",\"crops\" : ");
+        districtsAndCropsJson.append(districtsAndCrops[1]);
+        districtsAndCropsJson.append("}");
+		return districtsAndCropsJson.toString();
+	}
+
+	/**
+     * This authenticates and sets up a service stub for webservice calls
+     * 
+     * @return GetDistrictsAndCrops service stub
+     * @throws ServiceException
+     * @throws RemoteException
+     * @throws InvalidIdFault
+     * @throws UnexpectedErrorFault
+     * @throws LoginFault
+     */
+    private GetDistrictsAndCropsBindingStub setupSalesforceAuthentication() throws Exception {
+
+        GetDistrictsAndCropsServiceLocator getDistrictsAndCropsServiceLocator = new GetDistrictsAndCropsServiceLocator();
+        GetDistrictsAndCropsBindingStub serviceStub = (GetDistrictsAndCropsBindingStub)getDistrictsAndCropsServiceLocator.getGetDistrictsAndCrops();
+
+        // Use soap api to login and get session info
+        SforceServiceLocator soapServiceLocator = new SforceServiceLocator();
+        soapServiceLocator.setSoapEndpointAddress((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceAddress", ""));
+        SoapBindingStub binding = (SoapBindingStub)soapServiceLocator.getSoap();
+        LoginResult loginResult = binding.login((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceUsername", ""),
+                (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforcePassword", "")
+                        + (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceToken", ""));
+        SessionHeader sessionHeader = new SessionHeader(loginResult.getSessionId());
+
+        // Share the session info with our webservice
+        serviceStub.setHeader("http://soap.sforce.com/schemas/class/GetDistrictsAndCrops", "SessionHeader", sessionHeader);
+        return serviceStub;
+    }
 
 }
--- src/applab/farmerlink/server/GetFarmersAndMarketPrices.java
+++ src/applab/farmerlink/server/GetFarmersAndMarketPrices.java
@@ -1,10 +1,24 @@
 package applab.farmerlink.server;
 
+import applab.server.ApplabConfiguration;
 import applab.server.ApplabServlet;
+import applab.server.WebAppId;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.RemoteException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.rpc.ServiceException;
+
+import com.sforce.soap.enterprise.LoginResult;
+import com.sforce.soap.enterprise.SessionHeader;
+import com.sforce.soap.enterprise.SforceServiceLocator;
+import com.sforce.soap.enterprise.SoapBindingStub;
+import com.sforce.soap.schemas._class.GetFarmersAndMarketPrices.GetFarmersAndMarketPricesBindingStub;
+import com.sforce.soap.schemas._class.GetFarmersAndMarketPrices.GetFarmersAndMarketPricesServiceLocator;
 
 /**
  * Servlet implementation class GetFarmersAndMarketPrices
@@ -20,17 +34,59 @@ public class GetFarmersAndMarketPrices extends ApplabServlet {
     }
 
 	/**
+	 * @throws Exception 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doApplabGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		doApplabPost(request, response);
 	}
 
 	/**
+	 * @throws Exception 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String selectedDistrict = request.getHeader("district");
+		String selectedCrop = request.getHeader("crop");
+		String jsonResult = getFarmersAndMarketPricesFromSalesforce(selectedDistrict, selectedCrop);
+		PrintWriter sendResponse = response.getWriter();
+		sendResponse.println(jsonResult);
+	}
+
+	private String getFarmersAndMarketPricesFromSalesforce(
+			String selectedDistrict, String selectedCrop) throws Exception {
+		GetFarmersAndMarketPricesBindingStub serviceStub = setupSalesforceAuthentication();
+		String[] farmersAndMarketPrices = serviceStub.getFarmersAndMarketPrices(selectedDistrict, selectedCrop);
+		String jsonString = createJson(farmersAndMarketPrices);
+		return jsonString;
+	}
+
+	private String createJson(String[] farmersAndMarketPrices) {
+        StringBuffer farmersAndMarketPricesJson = new StringBuffer();
+        farmersAndMarketPricesJson.append("{\"farmers\" : ");
+        farmersAndMarketPricesJson.append(farmersAndMarketPrices[0]);
+        farmersAndMarketPricesJson.append(",\"marketprices\" : ");
+        farmersAndMarketPricesJson.append(farmersAndMarketPrices[1]);
+        farmersAndMarketPricesJson.append("}");
+		return farmersAndMarketPricesJson.toString();
+	}
+
+	private GetFarmersAndMarketPricesBindingStub setupSalesforceAuthentication() throws Exception {
+        GetFarmersAndMarketPricesServiceLocator getFarmersAndMarketPricesServiceLocator = new GetFarmersAndMarketPricesServiceLocator();
+        GetFarmersAndMarketPricesBindingStub serviceStub = (GetFarmersAndMarketPricesBindingStub)getFarmersAndMarketPricesServiceLocator.getGetFarmersAndMarketPrices();
+
+        // Use soap api to login and get session info
+        SforceServiceLocator soapServiceLocator = new SforceServiceLocator();
+        soapServiceLocator.setSoapEndpointAddress((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceAddress", ""));
+        SoapBindingStub binding = (SoapBindingStub)soapServiceLocator.getSoap();
+        LoginResult loginResult = binding.login((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceUsername", ""),
+                (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforcePassword", "")
+                        + (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceToken", ""));
+        SessionHeader sessionHeader = new SessionHeader(loginResult.getSessionId());
+
+        // Share the session info with our webservice
+        serviceStub.setHeader("http://soap.sforce.com/schemas/class/GetFarmersAndMarketPrices", "SessionHeader", sessionHeader);
+        return serviceStub;
 	}
 
 }
